79696a22e4bc6d88be427c85ecf6a187bc5a680e
maksmolchdmitr <maksmolchdmitr@gmail.com>
2024-09-03 22:55:42 +0300

    add lombok

--- src/main/java/maks/molch/dmitr/wsexample/model/ChatMessage.java
+++ src/main/java/maks/molch/dmitr/wsexample/model/ChatMessage.java
@@ -1,5 +1,8 @@
 package maks.molch.dmitr.wsexample.model;
 
+import lombok.Data;
+
+@Data
 public class ChatMessage {
     private String content;
     private String sender;
@@ -9,32 +12,6 @@ public class ChatMessage {
         CHAT,
         JOIN,
         LEAVE
-    }
-
-    // Геттеры и сеттеры
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public MessageType getType() {
-        return type;
-    }
-
-    public void setType(MessageType type) {
-        this.type = type;
     }
 }
 
